aabb4f1e27c3a887760a51323d364537ba58eaab
shalankwa <jonathangoodman.j@gmail.com>
2025-03-02 13:56:49 +0900

    fix go module name

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ Copyright © 2025 NAME HERE <EMAIL ADDRESS>
 */
 package main
 
-import "github.com/shalankwa/go-release-blueprint/cmd"
+import "github.com/Shalankwa/go-release-blueprint/cmd"
 
 func main() {
 	cmd.Execute()
